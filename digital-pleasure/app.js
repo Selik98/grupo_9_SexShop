c51b2f3d6f70c6759251eacac9124190b1915578
santiagocastillo01 <136028519+santiagocastillo01@users.noreply.github.com>
2023-07-04 20:31:25 -0300

    Update app.js

--- digital-pleasure/app.js
+++ digital-pleasure/app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static('public'));
 
 app.listen(7000, () => {
-    console.log ("Servidor en Puerto 5000 | http://localhost:7000")
+    console.log ("Servidor en Puerto 7000 | http://localhost:7000")
 }) 
 
 
