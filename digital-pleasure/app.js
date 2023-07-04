1e1edf59cb560e3ddb5fd2b8aac9ab03b84c6b4d
santiagocastillo01 <136028519+santiagocastillo01@users.noreply.github.com>
2023-07-04 20:10:56 -0300

    Update app.js

--- digital-pleasure/app.js
+++ digital-pleasure/app.js
@@ -1,12 +1,46 @@
 //requerimos express para usarlo
 const express = require('express');
 const path = require('path');
-
-const publicpath = path.resolve(__dirname, './public' ) ;
-app.use(express.static());
-
 //iniciamos un servidor y lo guardamos dentro de app
 const app = express();
 
+app.listen(7000, () => {
+    console.log ("Servidor en Puerto 5000 | http://localhost:7000")
+}) 
+
+
+app.get('/', (req,res) => {
+    
+    res.sendFile(path.join(__dirname, '/views/index.html'))
+});
+
+
+app.get('/login', (req,res) => {
+    
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+});
+
+
+app.get('/productCart', (req,res) => {
+    
+    res.sendFile(path.join(__dirname, '/views/productCart.html'))
+});
+
+
+app.get('/productDetail', (req,res) => {
+    
+    res.sendFile(path.join(__dirname, '/views/productDetail.html'))
+});
+
+
+app.get('/register', (req,res) => {
+    
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+});
+
+
+
+const publicpath = path.resolve(__dirname, './public' ) ;
+app.use(express.static());
 
 
