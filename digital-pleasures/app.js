48bff41ef00e2593d653fa5af628de5c456d34b6
melisa <melisabernalvilla@gmail.com>
2023-07-18 22:23:31 -0500

    hice cambios en index.css

--- digital-pleasures/app.js
+++ digital-pleasures/app.js
@@ -10,23 +10,23 @@ app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, './views/index.html'))
 });
 
-app.get('/login', (req,res) => {
-    console.log ('Hicieron un Request en "/login"')
+app.get('/login.html', (req,res) => {
+    console.log ('Hicieron un Request en "/login.html"')
     res.sendFile(path.join(__dirname, './views/login.html'))
 });
 
-app.get('/register', (req,res) => {
-    console.log ('Hicieron un Request en "/register"')
+app.get('/register.html', (req,res) => {
+    console.log ('Hicieron un Request en "/register.html"')
     res.sendFile(path.join(__dirname, './views/register.html'))
 });
 
-app.get('/cart', (req,res) => {
-    console.log ('Hicieron un Request en "/cart"')
+app.get('/cart.html', (req,res) => {
+    console.log ('Hicieron un Request en "/cart.html"')
     res.sendFile(path.join(__dirname, './views/cart.html'))
 });
 
-app.get('/detail', (req,res) => {
-    console.log ('Hicieron un Request en "/detail"')
+app.get('/detail.html', (req,res) => {
+    console.log ('Hicieron un Request en "/detail.html"')
     res.sendFile(path.join(__dirname, './views/detail.html'))
 });
 
