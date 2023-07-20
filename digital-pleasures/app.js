219308f95c5d673891651fa88e3dc2690f507bc6
Selik98 <mkiesel98@gmail.com>
2023-07-20 11:35:44 -0300

    Arreglé El LocalHost y puse Href en el Navbar

--- digital-pleasures/app.js
+++ digital-pleasures/app.js
@@ -1,6 +1,8 @@
 // npm install express
 const express = require ('express')
 const path = require ('path')
+const dotenv = require('dotenv').config();
+
 
 const app = express()
 
@@ -10,28 +12,28 @@ app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, './views/index.html'))
 });
 
-app.get('/login.html', (req,res) => {
+app.get('/login', (req,res) => {
     console.log ('Hicieron un Request en "/login.html"')
     res.sendFile(path.join(__dirname, './views/login.html'))
 });
 
-app.get('/register.html', (req,res) => {
+app.get('/register', (req,res) => {
     console.log ('Hicieron un Request en "/register.html"')
     res.sendFile(path.join(__dirname, './views/register.html'))
 });
 
-app.get('/cart.html', (req,res) => {
+app.get('/cart', (req,res) => {
     console.log ('Hicieron un Request en "/cart.html"')
     res.sendFile(path.join(__dirname, './views/cart.html'))
 });
 
-app.get('/detail.html', (req,res) => {
+app.get('/detail', (req,res) => {
     console.log ('Hicieron un Request en "/detail.html"')
     res.sendFile(path.join(__dirname, './views/detail.html'))
 });
 
 app.listen(process.env.PORT, () => {
-    console.log ("Servidor escuchando Puerto" + process.env.PORT + "http://localhost:3000")
+    console.log ("Servidor escuchando Puerto" + process.env.PORT + " http://localhost:3000")
 }) 
 
 app.use(express.static('public'));
