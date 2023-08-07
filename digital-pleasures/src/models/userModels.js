80c490d4124edfc0f27b41ab9330966b3d10a815
santiagocastillo01 <castillosantiago441@gmail.com>
2023-08-06 22:49:28 -0300

    modifique el userModels, y borre carpetas repetidas

--- digital-pleasures/src/models/userModels.js
+++ digital-pleasures/src/models/userModels.js
@@ -1,20 +1,51 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 
+const modelo = {
+    fileRoute: path.join(__dirname, '../data/users.json'),
 
+//CREAR USUARIO    
+    createUsers: (bodyData) => {
+        let users = modelo.findAll();
 
-const model = {
-    findAll: () => {
-        const data = fs.readFileSync(path.join(__dirname, "../data/users.json"), 'utf-8')
-        const users = JSON.parse(data)
-        console.table(users)
+        const lastUserId = users[users.length - 1].id;
+
+        const newUser = {
+            id: lastUserId + 1,
+            ...bodyData
+        };
+
+        users.push(newUser);
+
+        const jsonData = JSON.stringify(users);
+
+        fs.writeFileSync(modelo.fileRoute, jsonData, 'utf-8');
+
+        return newUser;
     },
-    findById: (id) => {
+//BORRAR USUARIO
+    destroy: (id) => {
+        let users = modelo.findAll();
 
+        users = users.filter(usuarioActual => usuarioActual.id !== id);
+
+        const jsonUsers = JSON.stringify(users);
+
+        fs.writeFileSync(modelo.fileRoute, jsonUsers, 'utf-8');
+    },
+//SUBIR EL USUARIO NUEVO 
+    updateUsers: (updatedUsers) => {
+
+        let users = modelo.findAll();
+
+        const prodIndex = users.findIndex(userActual => userActual.id === updatedUsers.id);
+
+        users[prodIndex] = updatedUsers;
+
+        const usersJson = JSON.stringify(users);
+
+        fs.writeFileSync(modelo.fileRoute, usersJson, 'utf-8');
     }
+};
 
-}
-
-module.exports = model;
-
-model.findAll()
+module.exports = modelo;
