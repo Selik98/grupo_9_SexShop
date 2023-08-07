29ec63d4d11c988df3c890a08fdee0e35fa5b21d
santiagocastillo01 <castillosantiago441@gmail.com>
2023-08-06 23:50:09 -0300

    modifique user.json y userModels

--- digital-pleasures/src/models/userModels.js
+++ digital-pleasures/src/models/userModels.js
@@ -1,12 +1,21 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
 
-const modelo = {
+
+
+const model = {
     fileRoute: path.join(__dirname, '../data/users.json'),
+    findAll: () => {
+        const data = fs.readFileSync(path.join(__dirname, "../data/users.json"), 'utf-8')
+        const users = JSON.parse(data)
+        console.table(users)
+    },
+    findById: (id) => {
 
-//CREAR USUARIO    
+    },
+//CREAR NUEVO USUARIO
     createUsers: (bodyData) => {
-        let users = modelo.findAll();
+        let users = model.findAll();
 
         const lastUserId = users[users.length - 1].id;
 
@@ -19,24 +28,24 @@ const modelo = {
 
         const jsonData = JSON.stringify(users);
 
-        fs.writeFileSync(modelo.fileRoute, jsonData, 'utf-8');
+        fs.writeFileSync(model.fileRoute, jsonData, 'utf-8');
 
         return newUser;
     },
 //BORRAR USUARIO
     destroy: (id) => {
-        let users = modelo.findAll();
+        let users = model.findAll();
 
         users = users.filter(usuarioActual => usuarioActual.id !== id);
 
         const jsonUsers = JSON.stringify(users);
 
-        fs.writeFileSync(modelo.fileRoute, jsonUsers, 'utf-8');
+        fs.writeFileSync(model.fileRoute, jsonUsers, 'utf-8');
     },
 //SUBIR EL USUARIO NUEVO 
     updateUsers: (updatedUsers) => {
 
-        let users = modelo.findAll();
+        let users = model.findAll();
 
         const prodIndex = users.findIndex(userActual => userActual.id === updatedUsers.id);
 
@@ -44,8 +53,11 @@ const modelo = {
 
         const usersJson = JSON.stringify(users);
 
-        fs.writeFileSync(modelo.fileRoute, usersJson, 'utf-8');
+        fs.writeFileSync(model.fileRoute, usersJson, 'utf-8');
     }
 };
 
-module.exports = modelo;
+
+module.exports = model;
+
+model.findAll()
