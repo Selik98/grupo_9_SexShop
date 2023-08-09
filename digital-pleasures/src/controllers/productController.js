49f57432e990016336f265d2e7a3756b2bade7f1
Selik98 <mkiesel98@gmail.com>
2023-08-08 21:48:45 -0300

    Solucioné El error de controllers

--- digital-pleasures/src/controllers/productController.js
+++ digital-pleasures/src/controllers/productController.js
@@ -1,6 +1,5 @@
 const path = require('path')
-const productModels = require('../models/productModels');
-const model = productModels()
+const model = require('../models/productModels');
 
 const controller = {
     cart: ('/cart', (req, res) => {
@@ -15,14 +14,14 @@ const controller = {
 
         const productModel = model.findById({ id })
         console.table(model)
-        res.render('detalleMenu', { model })
+        res.render('detalleMenu', { model: products })
     }),
-    update: ('/update',(req,res) =>{
+    update: ('/update', (req, res) => {
         const updatedProduct = req.body;
-        updatedProduct.id = Number(req.params.id)    
+        updatedProduct.id = Number(req.params.id)
         model.updateProduct(updatedProduct)
-        res.redirect('/products/'+ updatedProduct.id + '/detail')
-    }) 
+        res.redirect('/products/' + updatedProduct.id + '/detail')
+    })
     ,
     upload: ('/upload', (req, res) => {
         res.render('upload')
